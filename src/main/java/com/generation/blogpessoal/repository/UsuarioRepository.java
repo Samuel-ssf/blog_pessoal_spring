76471df721e676a40eeb69fd3b4e8bf82116b410
Samuel <samueldjsferreira@gmail.com>
2025-05-28 00:29:07 -0300

    :hammer: Correção pom.xml

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -8,7 +8,8 @@ import com.generation.blogpessoal.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	Optional<Usuario> findByUsuario(String usuario);
+	public Optional<Usuario> findByUsuario(String usuario);
+	
 	
 	/* SELECT * FROM tb_usuarios WHERE usuario = ? */
 }
